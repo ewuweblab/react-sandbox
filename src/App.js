803a9b9b4c9bcdc29b1bfa0bff2ec9c437fa6e01
EWU Web.Lab <pmanikoth@ewu.edu>
2020-06-02 14:14:42 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React Sandbox</h1>
+      <h2>Codesandbox is cool.</h2>
+    </div>
+  );
+}
